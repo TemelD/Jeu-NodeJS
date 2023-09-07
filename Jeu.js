2d003e2a7e07d9550a9d7a42cda304a36d46f7a7
TemelD <71669346+TemelD@users.noreply.github.com>
2023-09-07 12:34:36 +0200

    finish Project

--- Jeu.js
+++ Jeu.js
@@ -1,1 +1,49 @@
-//INIT
+const readline = require('readline-sync');
+
+const mots = ['pendu', 'voiture', 'isitech', 'mounir', 'temel', 'bonjour'];
+
+const motAleatoire = mots[Math.floor(Math.random() * mots.length)];
+
+let motAffiche = '';
+let lettresDevinees = [];
+let essaisRestants = 8;
+
+for (let i = 0; i < motAleatoire.length; i++) {
+    motAffiche += '_';
+  }
+
+  while (essaisRestants > 0) {
+    console.log(`Lettre déjà deviné: ${lettresDevinees.join(', ')}`);
+    console.log(`Il te reste : ${essaisRestants} essai`);
+    console.log(`MOT A DEVINER : ${motAffiche}`);
+    
+    const lettre = readline.question('Met une lettre : ');
+  
+    if (lettresDevinees.includes(lettre)) {
+      console.log('Tu as déjà mis cette lettre.');
+      continue;
+    }
+  
+    let lettreTrouvee = false;
+    for (let i = 0; i < motAleatoire.length; i++) {
+      if (motAleatoire[i] === lettre) {
+        motAffiche = motAffiche.substring(0, i) + lettre + motAffiche.substring(i + 1);
+        lettreTrouvee = true;
+      }
+    }
+  
+    if (!lettreTrouvee) {
+      essaisRestants--;
+    }
+  
+    lettresDevinees.push(lettre);
+  
+    if (motAffiche === motAleatoire) {
+      console.log(`Bravo, tu as gagné. Le mot est ${motAleatoire}`);
+      break;
+    }
+  }
+  
+  if (essaisRestants === 0) {
+    console.log(`Désolé, tu as perdu. Le mot c'est ${motAleatoire}`);
+  }
